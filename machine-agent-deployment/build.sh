56cd4d02eaf16d5c336985e4fbc38e53d95bbea5
Hans Ashlock <hashlock@thousandeyes.com>
2020-08-13 22:41:50 -0700

    Remove Terraform deployment for now

--- machine-agent-deployment/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker build -t 000eyes/teappd-agent .
-docker image inspect 000eyes/teappd-agent | grep Size
-docker image inspect 000eyes/teappd-agent | grep 000eyes/teappd-agent
-if [ "$1" == "-p" ] ; then
-	docker push 000eyes/teappd-agent
-fi
--- machine-agent-deployment/docker/stoplocal.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-terraform init terraform/local > /dev/null
-terraform destroy -auto-approve terraform/local > /dev/null
